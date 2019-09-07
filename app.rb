d07850b2337a3e6ee21edd6b85061f3e5360ca9a
Chance Smith <chancesmith97@hotmail.com>
2019-09-07 21:42:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,4 +10,12 @@ class App < Sinatra::Base
     "My name is Chance."
   end
   
+  get '/hometown' do 
+    "My hometown is Rapid City."
+  end
+  
+  get '/favorite-song' do 
+    "My favorite song is Shortcuts and Deadends."
+  end
+  
 end
